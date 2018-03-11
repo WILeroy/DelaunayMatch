375e5c6fc45a4b455b7697daf07c0cbb84acbc81
wileroy <wileroy@163.com>
2018-03-11 15:55:18 +0800

    Update

--- qmake/CMakeFiles/delaunayDetect.dir/DependInfo.cmake
+++ qmake/CMakeFiles/delaunayDetect.dir/DependInfo.cmake
@@ -1,19 +1,7 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/leroy/code/DelaunayDetect/_c_trianglenet.cpp" "/home/leroy/code/DelaunayDetect/qmake/CMakeFiles/delaunayDetect.dir/_c_trianglenet.cpp.o"
-  "/home/leroy/code/DelaunayDetect/delaunaydetect.cpp" "/home/leroy/code/DelaunayDetect/qmake/CMakeFiles/delaunayDetect.dir/delaunaydetect.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include"
-  "/usr/local/include/opencv"
-  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
--- qmake/CMakeFiles/delaunayDetect.dir/cmake_clean.cmake
+++ qmake/CMakeFiles/delaunayDetect.dir/cmake_clean.cmake
@@ -1,11 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/delaunayDetect.dir/delaunaydetect.cpp.o"
-  "CMakeFiles/delaunayDetect.dir/_c_trianglenet.cpp.o"
-  "delaunayDetect.pdb"
-  "delaunayDetect"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/delaunayDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
